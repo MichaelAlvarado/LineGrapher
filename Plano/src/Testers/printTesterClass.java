f9c4f83f178f7145a3f5356e9a396a031ecd6af1
Michael Alvarado <michael.alvarado1@upr.edu>
2020-02-13 10:36:07 -0400

    print tester changed for testing purposes

--- Plano/src/Testers/printTesterClass.java
+++ Plano/src/Testers/printTesterClass.java
@@ -1,25 +1,22 @@
 package Testers;
 
 import Coordinates.CartesianCoordinates;
+import Coordinates.Coordinates;
 import Coordinates.PolarCoordinates;
 
 public class printTesterClass {
 
 	public static void main(String[] args) {
 		
-		PolarCoordinates test = new PolarCoordinates(5.39, 68.20);
-		double x = PolarCoordinates.changePolarToX(test.getR(), test.getO());
-		double y = PolarCoordinates.changePolarToY(test.getR(), test.getO());
+		Coordinates test = new PolarCoordinates(5.39, 68.20);
 		
-		System.out.println("The x coordinate in Cartesian is: " + x);
-		System.out.println("The y coordintate in Cartesian is: " + y);
+		System.out.println("The x coordinate in Cartesian is: " + test.getX());
+		System.out.println("The y coordintate in Cartesian is: " + test.getY());
 		
-		CartesianCoordinates test2 = new CartesianCoordinates(2.0, 5.0);
-		double r = CartesianCoordinates.changeCartesianToMagnitud(test2.getX(), test2.getY());
-		double O = CartesianCoordinates.changeCartesianToAngle(test2.getX(), test2.getY());
+		Coordinates test2 = new CartesianCoordinates(2.0, 5.0);
 		
-		System.out.println("The r magnitude in Polar is: " + r);
-		System.out.println("The angle in Polar is: " + O);
+		System.out.println("The r magnitude in Polar is: " + test2.getR());
+		System.out.println("The angle in Polar is: " + test2.getO());
 		
 	}
 
